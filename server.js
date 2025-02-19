4eec6d5685cd58cf2383dec795093e15709ebefe
your_user_namer <urvashigolu29@gmail.com>
2025-02-19 22:39:43 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Application is running at 3000");
+});
+
+//In real time, this data will come from DB, for now I am creating it
+const person = [
+  { id: 2, name: "Alina", age: 20, city: "Mumbai", salary: 20000 },
+  {
+    id: 1,
+    name: "Alaya",
+    age: 23,
+    city: "Banglore",
+    salary: 24000,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).json({ name: "alina" });
+});
+
+app.get("/employees", (req, res) => {
+  res.status(200).json({ data: person });
+});
